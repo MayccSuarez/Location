98b021cdc8fe14cbc363b4558e3d368a593b9e5c
= <=>
2018-08-31 11:39:00 -0500

    configurar permisos

--- app/src/main/java/com/maycc/location/MainActivity.kt
+++ app/src/main/java/com/maycc/location/MainActivity.kt
@@ -2,11 +2,21 @@ package com.maycc.location
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity() {
+
+    private val fineLocation   = android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val coarseLocation = android.Manifest.permission.ACCESS_COARSE_LOCATION
+
+    lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = FusedLocationProviderClient(this)
     }
+
+
 }
