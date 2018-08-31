2b3ad006618fbce85d4d906d2e6bd990e0fe249e
= <=>
2018-08-31 12:01:15 -0500

    implementar validación de permisos

--- app/src/main/java/com/maycc/location/MainActivity.kt
+++ app/src/main/java/com/maycc/location/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.maycc.location
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +20,29 @@ class MainActivity : AppCompatActivity() {
         fusedLocationClient = FusedLocationProviderClient(this)
     }
 
+    override fun onStart() {
+        super.onStart()
 
+        if (validatePermissions()) {
+            getLocation()
+        } else {
+            askPermissions()
+        }
+    }
+
+    private fun validatePermissions(): Boolean {
+        val isPermissionCoarseLocationGranted = ActivityCompat.
+                checkSelfPermission(this, coarseLocation) == PackageManager.PERMISSION_GRANTED
+        val isPermissionFineLocationGranted   = ActivityCompat.
+                checkSelfPermission(this, fineLocation) == PackageManager.PERMISSION_GRANTED
+
+        return isPermissionCoarseLocationGranted && isPermissionFineLocationGranted
+    }
+
+    private fun askPermissions() {
+    }
+
+    private fun getLocation() {
+
+    }
 }
