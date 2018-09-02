b764947e48b5a9a638d443c1c6a4e4657633ad27
= <=>
2018-09-02 12:24:55 -0500

    implementar ubicación constante

--- app/src/main/java/com/maycc/location/MainActivity.kt
+++ app/src/main/java/com/maycc/location/MainActivity.kt
@@ -10,6 +10,9 @@ import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.tasks.OnSuccessListener
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,19 +23,51 @@ class MainActivity : AppCompatActivity() {
     private val CODE_PERMISSION_LOCATION = 100
 
     lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fusedLocationClient = FusedLocationProviderClient(this)
+        initLocationRequest()
+    }
+
+    private fun initLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getConstantLocation() {
+        locationCallback = object: LocationCallback() {
+
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+                var latitude: Double
+                var longitude: Double
+
+                for (location in locationResult?.locations!!) {
+                    latitude = location.latitude
+                    longitude = location.longitude
+
+                    Toast.makeText(applicationContext, "$latitude  $longitude", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 
     override fun onStart() {
         super.onStart()
 
         if (validatePermissions()) {
-            getUltimateLocation()
+            // getUltimateLocation()
+            getConstantLocation()
         } else {
             askPermissions()
         }
